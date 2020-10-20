a43290355fe66477ab97f60adcd8a2e91d30ca3c
wangshuaijing <wangshuaijing@58.com>
2020-10-20 16:27:49 +0800

    springboot + springmvc，使用默认配置

--- src/main/java/com/zhqy/springboot/controller/IndexController.java
+++ src/main/java/com/zhqy/springboot/controller/IndexController.java
@@ -1,23 +1,21 @@
 package com.zhqy.springboot.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <h3></h3>
+ * <h3>测试</h3>
  *
  * @author wangshuaijing
  * @version 1.0.0
- * @CreateDate 2020/10/16
+ * @date 2020/10/20 4:15 下午
  */
-@Controller
+@RestController
 public class IndexController {
 
-    @RequestMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("name", "aaa");
-        return "aaa";
+    @GetMapping("/hello")
+    public String index(){
+        return "hello word";
     }
 
 }
